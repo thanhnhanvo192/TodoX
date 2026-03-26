a707aba44e9655d89a1305782d5b99bf8d17d771
thanhnhanvo192 <102230035@sv1.dut.udn.vn>
2026-03-26 13:44:22 +0700

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,30 @@ import taskRoutes from "./routes/taskRoutes.js";
 import { connectDB } from "./configs/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 // middleware
-app.use(cors({ orgin: "http://localhost:5173" }));
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
